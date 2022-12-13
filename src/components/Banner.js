17a6053fbce1de5ddd80ae6f042ca368ead7ba8f
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-12-12 22:08:57 -0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ const Banner = () => {
     return(
         <section className="banner" id="home">
             <Container>
+                <div></div>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my portfolio</span>
